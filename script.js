174dd81fc9f3283baebfbc3966d87878fbb94bb9
Nathan Moore <Wildcats25jn@gmail.com>
2024-08-23 17:25:57 -0400

    update 2

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll("[data-num]");
+const operationButtons = document.querySelectorAll("[data-op]");
+const equalsButton = document.querySelector("[data-equals]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const deleteButton = document.querySelector("[data-delete]");
+const prevOperandTextElement = document.querySelector("[data-prev-op]");
+const currentOperandTextElement = document.querySelector("[data-current-op]");
